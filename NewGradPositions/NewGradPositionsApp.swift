64ca9857f63d1c45093e14cc16b656bb40a6ecda
Saketh Renangi <rensaketh@gmail.com>
2023-08-15 18:38:51 -0400

    Added login page and fixed name for job view

--- NewGradPositions/NewGradPositionsApp.swift
+++ NewGradPositions/NewGradPositionsApp.swift
@@ -5,13 +5,34 @@
 //  Created by Saketh Renangi on 8/12/23.
 //
 
+import Amplify
+import Authenticator
+import AWSCognitoAuthPlugin
 import SwiftUI
 
 @main
 struct NewGradPositionsApp: App {
+    init() {
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+        } catch {
+            print("Unable to configure Amplify \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Authenticator { state in
+                VStack {
+                    Text("Hello, \(state.user.username)")
+                    Button("Sign out") {
+                        Task {
+                            await state.signOut()
+                        }
+                    }
+                }
+            }
         }
     }
 }
